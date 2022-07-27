4b56cf96c95e9198944b49591a3d88cf258cef3e
DEVA <devendraprakash.prakash@gmail.com>
2022-07-27 20:57:49 +0530

    Initial push

---
+++ AirShare/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace AirShare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(46, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "RECEIVE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(588, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(46, 438);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(708, 47);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Interrupt Transfer";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button4.Location = new System.Drawing.Point(46, 189);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(708, 47);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Choose A File";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(46, 138);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(708, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 258);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "No File Selected";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(544, 258);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(210, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Disconnected";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 370);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Received File Location :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 397);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(224, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "C:\\Users\\Loki\\Downloads\\AirShare";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(46, 299);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(708, 57);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "IP Address";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(371, 258);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "0MB/S";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button5.Location = new System.Drawing.Point(322, 61);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(159, 47);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "SEND";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(795, 515);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "AirShare";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button5;
+    }
+}
---
+++ AirShare/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace AirShare
+{
+
+
+    public partial class Form1 : Form
+    {
+
+        Thread thread = null, thread1,thread2;
+        TcpListener server = null;
+        TcpClient client = null;
+        volatile string IP = "127.0.0.1";
+        volatile int totalLength = 0;
+        volatile string[] strlist;
+        static volatile bool isClientRunning = true;
+        static volatile string selectedFileName = null;
+    
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeServices() { 
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                isClientRunning = true;
+                IP = GetLocalIPAddress();
+                thread = new Thread(RunServer);
+                thread.Start();
+                UpdateIP(IP);
+
+                thread1 = new Thread(UpdateService);
+                thread1.Start();
+
+                thread2 = new Thread(UpdateSpeed);
+                thread2.Start();
+            }
+            catch (Exception exception) {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        public void UpdateIP(string ip)
+        {
+            label5.Text = ip;
+        }
+
+        private  void RunServer()
+        {
+            try
+            {
+                server = new TcpListener(IPAddress.Parse(IP), 8888);
+                server.Start();  // this will start the server
+                this.Invoke(new Action(() => this.label2.Text = "SERVER STARTED"));
+
+                while (isClientRunning)   //we wait for a connection
+                {
+                    client = server.AcceptTcpClient();  //if a connection exists, the server will accept it
+                    this.Invoke(new Action(() => this.label2.Text = "SERVER CONNECTED"));
+                    NetworkStream ns = client.GetStream(); //networkstream is used to send/receive messages
+
+                    byte[] fileInfo = new byte[4096];
+                    int len = ns.Read(fileInfo, 0, 4096);
+                    string str = CleanMessage(fileInfo, len);
+                    char[] separator = { ','};
+                    strlist = str.Split(separator,2);
+
+                    this.Invoke(new Action(() => this.progressBar1.Visible = true));
+                    this.Invoke(new Action(() => this.progressBar1.Minimum = 0));
+                    this.Invoke(new Action(() => this.progressBar1.Maximum = (Convert.ToInt32(strlist[1]))+5),null);
+
+                 
+
+                    string path = "C:\\Users\\Loki\\Downloads\\AirShare\\" + strlist[0];
+
+                    using (FileStream fs = File.Create(path))
+                    
+                    Console.WriteLine("RECEIVING FILE");
+
+                    FileStream streem = new FileStream(path, FileMode.OpenOrCreate);
+                    totalLength = 0;
+                    byte[] msg = new byte[1024];     //the messages arrive as byte array
+                    int numByte = ns.Read(msg, 0, msg.Length);
+                    totalLength += numByte;
+                    this.Invoke(new Action(() => this.progressBar1.Value = (int)totalLength),null);
+                    try
+                    {
+                        while ((numByte != 0))
+                        {
+                            streem.Write(msg, 0, 1024);
+                            numByte = ns.Read(msg, 0, msg.Length);
+                            totalLength += numByte;
+                        }
+
+                        Console.WriteLine("FILE RECEIVED");
+                        this.Invoke(new Action(() => this.label2.Text = "FILE RECEIVED"));
+                        this.Invoke(new Action(() => this.progressBar1.Value = (int)Convert.ToInt32(strlist[1])));
+                        totalLength = 0;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        streem.Close();
+                        ns.Dispose();
+                    }
+                    
+                    ns.Dispose();
+                    streem.Close();
+                }
+            }
+            catch (Exception d)
+            {
+                Console.WriteLine(d.Message);
+            }
+        }
+
+
+        private void RunClient()
+        {
+            try
+            {
+             
+                    
+
+
+            }
+            catch (Exception d)
+            {
+                Console.WriteLine(d.Message);
+            }
+        }
+
+        private void UpdateService() {
+            
+            try
+            {
+                while (true)
+                {
+                    this.Invoke(new Action(() => this.progressBar1.Value = (int)totalLength));
+                    Thread.Sleep(500);
+                }
+            }
+            catch (Exception exce)
+            {
+                Console.WriteLine(exce.Message);
+            }
+        }
+
+        private void UpdateSpeed()
+        {
+
+            try
+            {
+                double start = 0;
+                double intVal = 0;
+                while (true)
+                {
+                    Thread.Sleep(900);
+                    double totalSize = totalLength - start;
+                    start = totalLength;
+                    if (totalSize > 1024) {
+                        totalSize = totalSize / 1024;
+                        if (totalSize>1024)
+                        {
+                            totalSize = totalSize / 1024;
+                            intVal = Math.Round(totalSize,2);
+                            this.Invoke(new Action(() => this.label6.Text = Convert.ToString(intVal) + "MB/S"));
+                        }
+                        else
+                        {
+                            intVal = Math.Round(totalSize,2);
+                            this.Invoke(new Action(() => this.label6.Text = Convert.ToString(intVal) + "KB/S"));
+                        }
+                    }
+                    else
+                    {
+                        intVal = Math.Round(totalSize);
+                        this.Invoke(new Action(() => this.label6.Text = Convert.ToString(intVal) + "Bits/S"));
+                    }
+                }
+            }
+            catch (Exception exce)
+            {
+                Console.WriteLine(exce.Message);
+            }
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+        private static string CleanMessage(byte[] bytes, int size)
+        {
+            string message = Encoding.Default.GetString(bytes);
+            string messageToPrint = null;
+            
+                for(int i = 0; i < size; i++)
+                {
+                    var nullChar = message[i];
+                   if(nullChar != '\0')
+                {
+                    messageToPrint += nullChar;
+                }
+                }
+         
+            return messageToPrint;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try {
+                isClientRunning = false;
+                server.Stop();
+                label2.Text = "ABORTED";
+            }
+            catch(Exception h)
+            {
+                Console.WriteLine(h.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client.Close();
+                label2.Text = "INTERRUPTED";
+            }catch(NullReferenceException nu)
+            {
+                Console.WriteLine(nu.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "C:\\";
+            //openFileDialog.Filter = ".mp3";
+            openFileDialog.FilterIndex = 0;
+            openFileDialog.RestoreDirectory = true;
+
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedFileName = openFileDialog.FileName;
+                Console.WriteLine(selectedFileName);
+                label1.Text = selectedFileName;
+            }
+        }
+    }
+    
+}
